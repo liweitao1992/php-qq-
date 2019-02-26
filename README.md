ea77be85a411d71ae1963a00483d39bf10795af4
liweitao <liweitao@airdoc.com>
2019-02-26 16:36:46 +0800

    debug

---
+++ README.md
@@ -0,0 +1,5 @@
+# php-Swift_Smtp-qq-
+
+1.配置Mail.class.php 账号
+2.index.php发送邮件示例
+3.目前仅支持qq 不同邮箱平台 端口号不同 qq为 465 smtp.qq.com
--- mail/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once ("vendor/Mail.class.php");
-$obj = new Mail();
-$result = $obj->sendMail("这是一个标题","这是一个内容",'邮箱地址');
-var_dump($result);
